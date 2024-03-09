8eb54d756b95939485c667ec3b98386d2ddd71c1
lunarxyz <139734540+DevisLunar@users.noreply.github.com>
2024-03-09 01:24:17 -0300

    keep-alive.py

--- keep-alive.py
+++ keep-alive.py
@@ -1,15 +1,19 @@
 # RESTARTER BY Revive#8798
 # WEBHOOK AND JSON SUPPORT BY Moonly#7996
 
+
 # modules
 import requests, subprocess, json
 from colorama import Fore, Style
 
-f = open("restarter-config.json")
+
+f = open("restarter-config.json", "r")
 data = json.load(f)
 f.close()
 
+
 minutes_per_restart = data['minutes']
+
 
 while 1:
     process = subprocess.Popen(['python', 'notifier-main.py'])
